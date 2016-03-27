0bcf3820fe2667e8fd588863aa0cc26e2aa03ac1
conradbarrett <conradbarrett@gmail.com>
2016-03-27 17:06:25 -0400

    import and updating username / password

---
+++ Deploy_software_wordpress_nginx-mysql56/attributes/default.rb
@@ -0,0 +1,8 @@
+# MySQL settings
+default["mysql"]["root"] = "tightropeio"
+default["mysql"]["pass"] = "t1ghtr0p3"
+default["mysql"]["user"] = "tightrope"
+
+# WP HTTP_ROOT
+default["phpapp"]["path"] = "/var/www/wordpress/"
+
